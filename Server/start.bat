1731b7bb62ae3b8fddbed16febe13dc6a7409e1d
xhaoh94 <xhaoh94@qq.com>
2024-01-22 18:54:21 +0800

    更新start.bat

--- Server/start.bat
+++ Server/start.bat
@@ -2,6 +2,9 @@
 @echo off
 set a=%cd%
 set d=%a%\uxgame.exe
+set e=%a%\etcd\etcd.exe
+
+tasklist|findstr /i %e%>nul &&echo "" || start %e%
 
 start %d% -appConf="./app_1.yaml"
 start %d% -appConf="./app_2.yaml"
